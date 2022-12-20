8b05888efd13fd3efd0fde2b0ab2f78087b63c67
SmithankumarR <smithankumar404@gmail.com>
2022-12-20 12:29:04 +0530

    dino game update

---
+++ dino-game/src/App.js
@@ -0,0 +1,11 @@
+import Dino from "./components/dino/Dino"
+function App() {
+  return (
+    <div className="App">
+      <h1>Dino Game</h1>
+      <Dino />
+    </div>
+  );
+}
+
+export default App;
